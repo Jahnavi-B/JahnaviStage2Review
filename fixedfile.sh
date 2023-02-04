2447498c4ace51ccdf3c42d40bc66869e908326a
Jahnavi-B <jahnavi21abc@gmail.com>
2023-02-04 19:23:59 +0530

    Added: flipcoinsimple.sh on flipingcoinsimple

--- fixedfile.sh
+++ fixedfile.sh
@@ -3,16 +3,12 @@
 Heads=0;
 Tails=0;
 
-until [ $Heads -gt 20] | [$Tails -gt 20 ]
-
-do
-
+while [ $Heads -lt 20] | [$Tails -lt 20 ];do
 	Result=$((RANDOM%2));
 	if [[ ${Result} -eq 0 ]]; then
     		Heads=$(($Heads + 1));
 	elif [[ ${Result} -eq 1 ]]; then
     		Tails=$(($Tails + 1));
-
 done
 
 echo "Heads=" $Heads;
